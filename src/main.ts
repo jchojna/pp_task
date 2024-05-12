c41a45783ff6e5d090143e8d62e859b89b9bc342
Jakub Chojna <jakub.chojna@gmail.com>
2024-05-12 19:24:43 +0200

    feat: don't use css variable for background y position

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,9 @@ const heroImageScrollFactor = 0.1;
 const imageScrollFactor = -0.25;
 
 // the hero image background y position - in pixels
-let backgroundYPosition =
+let heroImgBgYPosition =
   (heroImage && heroImage.computedStyleMap().get('background-position-y')) || 0;
-backgroundYPosition = parseInt(backgroundYPosition.toString());
+heroImgBgYPosition = parseInt(heroImgBgYPosition.toString());
 
 ctaLink.addEventListener('mousemove', (e: MouseEvent): void => {
   const { left, top } = ctaLink.getBoundingClientRect();
@@ -28,10 +28,9 @@ ctaLink.addEventListener('mousemove', (e: MouseEvent): void => {
 
 window.addEventListener('scroll', () => {
   if (heroImage && heroImage instanceof HTMLElement) {
-    heroImage.style.setProperty(
-      '--hero-img-top',
-      `${+backgroundYPosition + window.scrollY * heroImageScrollFactor}px`
-    );
+    heroImage.style.backgroundPositionY = `${
+      +heroImgBgYPosition + window.scrollY * heroImageScrollFactor
+    }px`;
   }
 
   if (header && header instanceof HTMLElement) {
