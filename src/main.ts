61d1e5a1b3135051dd87255759f111322c10ba8b
Jakub Chojna <jakub.chojna@gmail.com>
2024-05-12 13:52:56 +0200

    feat: add scroll effect to image

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ const ctaLink = document.querySelector('.content-cta-js');
 if (!ctaLink || !(ctaLink instanceof HTMLElement)) {
   throw new Error('ctaLink not found');
 }
+const heroImage = document.querySelector('.image-js');
 
 ctaLink.addEventListener('mousemove', (e: MouseEvent): void => {
   const { left, top } = ctaLink.getBoundingClientRect();
   ctaLink.style.setProperty('--cta-glow-left', `${e.clientX - left}px`);
   ctaLink.style.setProperty('--cta-glow-top', `${e.clientY - top}px`);
+});
+
+window.addEventListener('scroll', () => {
+  if (heroImage && heroImage instanceof HTMLElement) {
+    heroImage.style.transform = `translateY(${window.scrollY * 0.1}px)`;
+  }
 });
