e53d3822fff6576ec17949acd40a26bfe912ff00
Jakub Chojna <jakub.chojna@gmail.com>
2024-05-12 15:30:44 +0200

    feat: handle image scroll differently

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ const ctaLink = document.querySelector('.content-cta-js');
 if (!ctaLink || !(ctaLink instanceof HTMLElement)) {
   throw new Error('ctaLink not found');
 }
-const heroImage = document.querySelector('.image-js');
+const heroImage = document.querySelector('.hero-image-js');
 const header = document.querySelector('.header-js');
 const heroImageScrollFactor = 0.1;
 const imageScrollFactor = -0.25;
+
+// the hero image background y position - in pixels
+let backgroundYPosition =
+  (heroImage && heroImage.computedStyleMap().get('background-position-y')) || 0;
+backgroundYPosition = parseInt(backgroundYPosition.toString());
 
 ctaLink.addEventListener('mousemove', (e: MouseEvent): void => {
   const { left, top } = ctaLink.getBoundingClientRect();
@@ -25,10 +30,12 @@ ctaLink.addEventListener('mousemove', (e: MouseEvent): void => {
 
 window.addEventListener('scroll', () => {
   if (heroImage && heroImage instanceof HTMLElement) {
-    heroImage.style.transform = `translateY(${
-      window.scrollY * heroImageScrollFactor
-    }px)`;
+    heroImage.style.setProperty(
+      '--hero-img-top',
+      `${+backgroundYPosition + window.scrollY * heroImageScrollFactor}px`
+    );
   }
+
   if (header && header instanceof HTMLElement) {
     header.style.transform = `translateY(${
       window.scrollY * imageScrollFactor
