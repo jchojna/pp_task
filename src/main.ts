47d204b5638af3a35a1e84abf0746286dbfc6288
Jakub Chojna <jakub.chojna@gmail.com>
2024-05-12 13:43:13 +0200

    feat: handle cta link glow

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
 import './main.css';
 
-console.log('hello');
+const ctaLink = document.querySelector('.content-cta-js');
+if (!ctaLink || !(ctaLink instanceof HTMLElement)) {
+  throw new Error('ctaLink not found');
+}
+
+ctaLink.addEventListener('mousemove', (e: MouseEvent): void => {
+  const { left, top } = ctaLink.getBoundingClientRect();
+  ctaLink.style.setProperty('--cta-glow-left', `${e.clientX - left}px`);
+  ctaLink.style.setProperty('--cta-glow-top', `${e.clientY - top}px`);
+});
